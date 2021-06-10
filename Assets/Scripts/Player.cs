25755b8ad24ebb02da8f046bfdbe88d72ae3ca06
SergeyAD <gera-killer2005@rambler.ru>
2021-06-10 16:34:13 +0300

    Lesson1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        // Общий класс игрока
+
+        public int Lives { get; set; }
+
+
+    }
+}
